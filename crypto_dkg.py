129d232f3a9a781a54abda8a3035bc232bf5a624
Mouhamad Ndiankho THIAM <thiamouhamad@gmail.com>
2014-09-23 23:27:34 +0000

    adding the file crypto_dkg,py

---
+++ crypto_dkg.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+from string import maketrans
+from string import printable
+MAPPING=["éèêëçàöô","eeeecaoo"]# POUUR DES RAISONS DE TRANSFORMATION
+MYPRINTABLE=printable+MAPPING[0] #C'est pour la plage de lettres à utiliser
+#MYPRINTABLE=printable+''
+def reordonner_alphabet(phrase):
+  """Réordonner l'alphabet en utilisant la phrase comme mot clé"""
+  #phrase=phrase.lower()
+  #phrase=normaliser(phrase)
+  longueur=len(phrase)
+  resultat=""
+  alphabet=map(None,MYPRINTABLE)
+  for i in range(longueur):
+    if resultat.find(phrase[i])==-1: # la lettere n'est pas encore dans le résultat
+      resultat+=phrase[i] # on l'ajoute
+      alphabet.remove(phrase[i])
+  resultat+=''.join(alphabet)
+  return resultat
+def normaliser(texte):
+  """Elle permet de normaliser un texte en prenant en compte les caractère qu'il faut transformer"""
+  myTans=maketrans(MAPPING[0],MAPPING[1])
+  return texte.translate(myTans)
+def encoder_vigenere(texte,cle):
+  TRANS=maketrans(MYPRINTABLE,reordonner_alphabet(cle))
+  #texte=normaliser(texte)
+  return texte.lower().translate(TRANS)
+def decoder_vigenere(texte,cle):
+  TRANS=maketrans(reordonner_alphabet(cle),MYPRINTABLE)
+  return texte.translate(TRANS)
